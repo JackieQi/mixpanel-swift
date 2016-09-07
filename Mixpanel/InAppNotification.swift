fbe9ccf1f238a16568aa8812d414e59483fa6bf9
Jackie Qi <chi_qi@homedepot.com>
2016-09-07 15:41:55 -0700

    try to fix compiler error in demo with xcodebuild

--- Mixpanel/InAppNotification.swift
+++ Mixpanel/InAppNotification.swift
@@ -27,6 +27,100 @@ struct InAppNotification {
     let body: String
     let callToAction: String
     let callToActionURL: URL?
+
+    init?(JSONObject: [String: Any]?) {
+      guard let object = JSONObject else {
+        Logger.error(message: "notification json object should not be nil")
+        return nil
+      }
+
+      guard let ID = object["id"] as? Int, ID > 0 else {
+        Logger.error(message: "invalid notification id")
+        return nil
+      }
+
+      guard let messageID = object["message_id"] as? Int, messageID > 0 else {
+        Logger.error(message: "invalid notification message id")
+        return nil
+      }
+
+      guard let type = object["type"] as? String else { // todo check if its right types
+        Logger.error(message: "invalid notification type")
+        return nil
+      }
+
+      guard let style = object["style"] as? String else {
+        Logger.error(message: "invalid notification style")
+        return nil
+      }
+
+      guard let title = object["title"] as? String, !title.isEmpty else {
+        Logger.error(message: "invalid notification title")
+        return nil
+      }
+
+      guard let body = object["body"] as? String, !body.isEmpty else {
+        Logger.error(message: "invalid notification body")
+        return nil
+      }
+
+      guard let callToAction = object["cta"] as? String else {
+        Logger.error(message: "invalid notification cta")
+        return nil
+      }
+
+      var callToActionURL: URL?
+      if let URLString = object["cta_url"] as? String {
+        callToActionURL = URL(string: URLString)
+      }
+
+      guard let imageURLString = object["image_url"] as? String,
+        let escapedImageURLString = imageURLString
+          .addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed),
+        var imageURLComponents = URLComponents(string: escapedImageURLString) else {
+          Logger.error(message: "invalid notification image url")
+          return nil
+      }
+
+      if type == InAppType.Takeover.rawValue {
+        imageURLComponents.path = imageURLComponents.path.appendSuffixBeforeExtension(suffix: "@2x")
+      }
+
+      guard let imageURLParsed = imageURLComponents.url else {
+        Logger.error(message: "invalid notification image url")
+        return nil
+      }
+
+      self.init(ID: ID,
+                messageID: messageID,
+                type: type,
+                style: style,
+                imageURL: imageURLParsed,
+                title: title,
+                body: body,
+                callToAction: callToAction,
+                callToActionURL: callToActionURL)
+    }
+
+    init(ID: Int,
+         messageID: Int,
+         type: String,
+         style: String,
+         imageURL: URL,
+         title: String,
+         body: String,
+         callToAction: String,
+         callToActionURL: URL? = nil) {
+        self.ID = ID
+        self.messageID = messageID
+        self.type = type
+        self.style = style
+        self.imageURL = imageURL
+        self.title = title
+        self.body = body
+        self.callToAction = callToAction
+        self.callToActionURL = callToActionURL
+    }
 }
 
 extension String {
@@ -43,82 +137,5 @@ extension String {
             Logger.error(message: "cannot add suffix to URL string")
         }
         return newString
-    }
-}
-
-extension InAppNotification {
-    init?(JSONObject: [String: Any]?) {
-        guard let object = JSONObject else {
-            Logger.error(message: "notification json object should not be nil")
-            return nil
-        }
-
-        guard let ID = object["id"] as? Int, ID > 0 else {
-            Logger.error(message: "invalid notification id")
-            return nil
-        }
-
-        guard let messageID = object["message_id"] as? Int, messageID > 0 else {
-            Logger.error(message: "invalid notification message id")
-            return nil
-        }
-
-        guard let type = object["type"] as? String else { // todo check if its right types
-            Logger.error(message: "invalid notification type")
-            return nil
-        }
-
-        guard let style = object["style"] as? String else {
-            Logger.error(message: "invalid notification style")
-            return nil
-        }
-
-        guard let title = object["title"] as? String, !title.isEmpty else {
-            Logger.error(message: "invalid notification title")
-            return nil
-        }
-
-        guard let body = object["body"] as? String, !body.isEmpty else {
-            Logger.error(message: "invalid notification body")
-            return nil
-        }
-
-        guard let callToAction = object["cta"] as? String else {
-            Logger.error(message: "invalid notification cta")
-            return nil
-        }
-
-        var callToActionURL: URL?
-        if let URLString = object["cta_url"] as? String {
-            callToActionURL = URL(string: URLString)
-        }
-
-        guard let imageURLString = object["image_url"] as? String,
-            let escapedImageURLString = imageURLString
-                .addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed),
-            var imageURLComponents = URLComponents(string: escapedImageURLString) else {
-            Logger.error(message: "invalid notification image url")
-            return nil
-        }
-
-        if type == InAppType.Takeover.rawValue {
-            imageURLComponents.path = imageURLComponents.path.appendSuffixBeforeExtension(suffix: "@2x")
-        }
-
-        guard let imageURLParsed = imageURLComponents.url else {
-            Logger.error(message: "invalid notification image url")
-            return nil
-        }
-
-        self.init(ID: ID,
-                  messageID: messageID,
-                  type: type,
-                  style: style,
-                  imageURL: imageURLParsed,
-                  image: nil,
-                  title: title,
-                  body: body,
-                  callToAction: callToAction,
-                  callToActionURL: callToActionURL)
     }
 }
